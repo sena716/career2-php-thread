3a6978a5f8cef7ab4264d8d4cd109833de394edc
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-06-10 11:43:48 +0900

    自分のcodeに変更しました

--- index.php
+++ index.php
@@ -1,75 +1,61 @@
-<html>
-<head><title>掲示板</title></head>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sns</title>
+</head>
 <body>
+    <h1>掲示板App</h1>
+    <h2>投稿フォーム</h2>
 
-<h1>掲示板App</h1>
+    <form action="" method="post">
 
-<h2>投稿フォーム</h2>
+        <input type="text" name="name" placeholder="名前" required><br><br>
+        
+        <textarea name="message" id="" cols="30" rows="10" placeholder="内容" required></textarea><br><br>
+        <input type="submit" value="投稿する" name="entry">
+    </form>
 
-<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-    <input type="text" name="personal_name" placeholder="名前" required><br><br>
-    <textarea name="contents" rows="8" cols="40" placeholder="内容" required>
-</textarea><br><br>
-    <input type="submit" name="btn" value="投稿する">
-</form>
+    <form action="" method="post">
+        <input type="submit" value="削除する" name="remove">
+    </form>
 
-<h2>スレッド</h2>
+    <h2>スレッド</h2>
 
-<?php
-
-const THREAD_FILE = 'thread.txt';
-
-function readData() {
-    // ファイルが存在しなければデフォルト空文字のファイルを作成する
-    if (! file_exists(THREAD_FILE)) {
-        $fp = fopen(THREAD_FILE, 'w');
-        fwrite($fp, '');
-        fclose($fp);
-    }
-
-    $thread_text = file_get_contents(THREAD_FILE);
-    echo $thread_text;
-}
-
-function writeData() {
-    $personal_name = $_POST['personal_name'];
-    $contents = $_POST['contents'];
-    $contents = nl2br($contents);
-
-    $data = "<hr>\n";
-    $data = $data."<p>投稿者:".$personal_name."</p>\n";
-    $data = $data."<p>内容:</p>\n";
-    $data = $data."<p>".$contents."</p>\n";
-
-    $fp = fopen(THREAD_FILE, 'a');
-
-    if ($fp){
-        if (flock($fp, LOCK_EX)){
-            if (fwrite($fp,  $data) === FALSE){
-                print('ファイル書き込みに失敗しました');
+    <?php
+        // date.txtを作成
+        function filecreate(){
+            if( !file_exists('date.txt')){
+                touch( 'date.txt' );
             }
-
-            flock($fp, LOCK_UN);
-        }else{
-            print('ファイルロックに失敗しました');
         }
-    }
 
-    fclose($fp);
+        // ファイル読み込み
+        function filerelod(){
+            // $fp = fopen( "date.txt", "r" );
+            // while( $value = fgets( $fp )){
+            //     echo "$value<br>";
+            // }
+            // fclose( $fp );
+            echo file_get_contents( "date.txt" );
+        }
 
-    // ブラウザのリロード対策
-    $redirect_url = $_SERVER['HTTP_REFERER'];
-    header("Location: $redirect_url");
-    exit;
-}
+        // 投稿ボタンイベント
+        if( isset( $_POST['entry'] )){
+            $value = '<hr>'.'投稿日時:'.date("Y/m/d H:i:s").'<br>'.'投稿者:'.$_POST['name'].'<br>'.'内容:'.'<br>'.$_POST['message'];
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    writeData();
-}
+            filecreate();
+            file_put_contents( "date.txt", $value );
+            filerelod();
 
-readData();
+        } else if( isset( $_POST['remove'] )){
 
-?>
+            $fp = fopen( "date.txt", "w" );
+            fclose( $fp );
+            filerelod();
+        }
+    ?>
 
 </body>
 </html>
