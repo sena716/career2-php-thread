a00aafd0f33dafe8051c7ba54f549df0f3a683f9
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-06-17 11:02:26 +0900

    index.php更新しました

--- index.php
+++ index.php
@@ -6,16 +6,11 @@
     <title>sns</title>
 </head>
 <body>
-
-    <style></style>
-
     <h1>掲示板App</h1>
     <h2>投稿フォーム</h2>
 
     <form action="" method="post">
-
         <input type="text" name="name" placeholder="名前" required><br><br>
-        
         <textarea name="message" id="" cols="30" rows="10" placeholder="内容" required></textarea><br><br>
         <input type="submit" value="投稿する" name="entry">
     </form>
@@ -27,7 +22,6 @@
     <h2>スレッド</h2>
 
     <?php
-
         // 保存file名
         const FILE_NAME = "date.txt";
 
@@ -49,6 +43,10 @@
                 $fp = fopen( FILE_NAME, "a" );
                 fwrite( $fp, $value );
                 fclose( $fp );
+
+                $redirect_url = $_SERVER['HTTP_REFERER'];
+                header("Location: $redirect_url");
+                exit;
             } else {
                 file_put_contents( FILE_NAME, "" );
             }
@@ -56,13 +54,14 @@
 
         // 投稿ボタンイベント
         if( isset( $_POST['entry'] )){
+
             date_default_timezone_set('Asia/Tokyo');
             $value = '<hr>'.'投稿日時:'.date("Y/m/d H:i:s").'<br>'.'投稿者:'.$_POST['name'].'<br>'.'内容:'.'<br>'.$_POST['message'];
 
             filecreate();
             fileAction( $value );
-        } else if( isset( $_POST['remove'] )){
 
+        } else if( isset( $_POST['remove'] )){
             fileAction();
         }
 
